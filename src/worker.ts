f590fd15c0605b6884699ec3dad15fd84b3a1236
yang <yyyanghj@gmail.com>
2021-11-16 00:33:03 +0800

    feat: use synckit instead of deasync (#3)

---
+++ src/worker.ts
@@ -0,0 +1,25 @@
+import { getHighlighter, IThemeRegistration, ILanguageRegistration, Highlighter } from 'shiki'
+import { runAsWorker } from 'synckit'
+
+let h: Highlighter
+
+function handler(command: 'getHighlighter', options: {
+  themes: IThemeRegistration[]
+  langs: ILanguageRegistration[]
+}): void
+function handler(command: 'codeToHtml', options: {
+  code: string
+  lang: string
+  theme: string | undefined
+}): Promise<string>
+async function handler(command: 'getHighlighter' | 'codeToHtml', options: any) {
+  if (command === 'getHighlighter') {
+    h = await getHighlighter(options)
+  }
+  else if (command === 'codeToHtml') {
+    const { code, lang, theme } = options
+    return h.codeToHtml(code, lang, theme)
+  }
+}
+
+runAsWorker(handler)
