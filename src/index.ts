0c068f95da28acdee0f9b1928eb85abf9ce9e409
TANIGUCHI Masaya <ta2gch@gmail.com>
2022-07-06 06:29:58 +0000

    Synckit to Unasync

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { createRequire } from 'module'
 import type { Highlighter, ILanguageRegistration, IShikiTheme, IThemeRegistration } from 'shiki'
 import type MarkdownIt from 'markdown-it'
-import { createSyncFn } from 'synckit'
+import { createSyncFn } from 'unasync'
 
 export interface DarkModeThemes {
   dark: IThemeRegistration
