6473b3ead54e673bc3704c46db25f2e0a0c54dd1
Rasso Hilber <mail@rassohilber.com>
2023-05-04 09:07:07 +0200

    Explain the the reasoning behind the `<pre hidden>` element at the beginning of the output

--- src/index.ts
+++ src/index.ts
@@ -122,6 +122,7 @@ const MarkdownItShiki: MarkdownIt.PluginWithOptions<Options> = (markdownit, opti
         .replace('<pre class="shiki ', '<pre class="shiki shiki-dark ')
       const light = highlightCode(code, lang, darkModeThemes.light, lineOptions)
         .replace('<pre class="shiki ', '<pre class="shiki shiki-light ')
+      // The leading <pre hidden> prevents markdown-it from wrapping the output.
       return `<pre hidden></pre><div class="shiki-container language-${lang}">${dark}${light}</div>`
     }
     else {
